f3b1646516b49ab2a56f693e38eaf0dffd45f411
sarapaolucci <paolucci.sara@franchettisalviani.net>
2026-04-21 10:32:38 +0200

    Metodi corri e esci per la classe scambio

--- Staffetta/src/staffetta/Testimone.java
+++ Staffetta/src/staffetta/Testimone.java
@@ -11,6 +11,24 @@ package staffetta;
 public class Testimone {
     private boolean inCorsa;
     
+    public synchronized void corri() throws InterruptedException{
+        while(inCorsa){
+            try{
+                System.out.println("aspetto testimone...");
+                wait();
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println("Ho il testimone");
+            inCorsa = true;
+        }
+    }
     
+    public synchronized void esci(){
+        inCorsa = false;
+        System.out.println("Lascio il testimone");
+        notifyAll();
+    }
     
 }
