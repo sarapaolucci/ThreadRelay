52834f226f914e9245fdb2d20ddf915cc6691ab4
sarapaolucci <paolucci.sara@franchettisalviani.net>
2026-04-21 10:25:31 +0200

    Creazione classi

---
+++ Staffetta/src/staffetta/Runner.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package staffetta;
+
+/**
+ *
+ * @author paolucci.sara
+ */
+public class Runner extends Thread{
+    private Testimone t;
+    private String nome;
+    
+    public Runner(String n){
+        this.nome = n;
+    }
+    
+    
+}
